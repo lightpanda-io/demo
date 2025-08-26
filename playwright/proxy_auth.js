26c0403a12be4dbdf0f7b1d38a9ae1a568130cde
Pierre Tachoire <pierre@lightpanda.io>
2025-08-26 11:42:45 +0200

    add playwright example for auth

---
+++ playwright/proxy_auth.js
@@ -0,0 +1,63 @@
+// Copyright 2023-2024 Lightpanda (Selecy SAS)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Import the Chromium browser into our scraper.
+import { chromium } from 'playwright';
+
+// browserAddress
+const browserAddress = process.env.BROWSER_ADDRESS ? process.env.BROWSER_ADDRESS : 'ws://127.0.0.1:9222';
+
+// web serveur url
+const baseURL = process.env.BASE_URL ? process.env.BASE_URL : 'http://127.0.0.1:1234';
+
+// measure general time.
+const gstart = process.hrtime.bigint();
+// store all run durations
+let metrics = [];
+
+// Connect to an existing browser
+console.log("Connection to browser on " + browserAddress);
+const browser = await chromium.connectOverCDP({
+    endpointURL: browserAddress,
+    logger: {
+      isEnabled: (name, severity) => true,
+      log: (name, severity, message, args) => console.log(`${name} ${message}`)
+    }
+});
+
+
+const context = await browser.newContext({
+    baseURL: baseURL,
+    proxy: {
+      server: process.env.PROXY_ADDRESS ? process.env.PROXY_USERNAME : 'http://127.0.0.1:3000',
+      username: process.env.PROXY_USERNAME,
+      password: process.env.PROXY_PASSWORD
+    },
+});
+
+const page = await context.newPage();
+await page.goto('/campfire-commerce/');
+const html = await page.content();
+
+if (html.substring(0, 20) !== "<!DOCTYPE html><html") {
+  console.log(html.substring(0, 20));
+  throw new Error("html content is not as expected");
+}
+console.log(html);
+
+await page.close();
+await context.close();
+
+// Turn off the browser to clean up after ourselves.
+await browser.close();
