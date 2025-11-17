32da255d8075116147900e84cb417f212f025687
Pierre Tachoire <pierre@lightpanda.io>
2025-11-17 10:01:09 +0100

    add rod example

--- runner/main.go
+++ runner/main.go
@@ -121,6 +121,7 @@ func run(ctx context.Context, args []string, stdout, stderr io.Writer) error {
 		{Bin: "go", Args: []string{"run", "click/main.go", "http://127.0.0.1:1234/"}, Dir: "chromedp"},
 		{Bin: "go", Args: []string{"run", "ri/main.go", "http://127.0.0.1:1234/campfire-commerce/"}, Dir: "chromedp"},
 		{Bin: "go", Args: []string{"run", "fromnode/main.go", "http://127.0.0.1:1234/campfire-commerce/"}, Dir: "chromedp"},
+		{Bin: "go", Args: []string{"run", "dump/main.go", "http://127.0.0.1:1234/campfire-commerce/"}, Dir: "rod"},
 
 		// add fetch + tls mostly for proxy
 		{Bin: "node", Args: []string{"puppeteer/dump.js"}, Env: []string{"URL=https://demo-browser.lightpanda.io/campfire-commerce/"}},
