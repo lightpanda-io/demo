9e0c3c90469e2f51e0b746e89971b78765169274
Pierre Tachoire <pierre@lightpanda.io>
2026-01-09 09:49:21 +0100

    add integration for README and Quickstart
    Ensure the README and quickstart examples are working.

---
+++ integration/quickstart_wikipedia.js
@@ -0,0 +1,54 @@
+// Copyright 2023-2025 Lightpanda (Selecy SAS)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+'use strict'
+
+import puppeteer from 'puppeteer-core';
+
+// This is a copy of the Quickstart example.
+// The test ensures the example is working.
+
+// use browserWSEndpoint to pass the Lightpanda's CDP server address.
+const browser = await puppeteer.connect({
+  browserWSEndpoint: "ws://127.0.0.1:9222",
+});
+
+// The rest of your script remains the same.
+const context = await browser.createBrowserContext();
+const page = await context.newPage();
+
+await page.goto('https://en.wikipedia.org/wiki/Web_browser');
+
+// Extract all links from the references list of the page.
+const links = await page.evaluate(() => {
+  return Array.from(document.querySelectorAll('.references a.external')).map(row => {
+    return row.getAttribute('href');
+  });
+});
+
+await page.close();
+await context.close();
+await browser.disconnect();
+
+let found = false;
+for (const link of links) {
+  if (link === 'https://radar.cloudflare.com/reports/browser-market-share-2024-q1') {
+    found = true;
+    break;
+  }
+}
+
+if (!found) {
+  console.log("Failed to find expected links");
+  throw new Error("invalid results");
+}
