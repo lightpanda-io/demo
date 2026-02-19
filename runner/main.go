b1120ad28712192bb916b8a768a8df39960791c1
Karl Seguin <k@openmymind.io>
2026-02-19 13:43:20 +0800

    Adds a basic puppeteer test for frames
    Depends on https://github.com/lightpanda-io/browser/pull/1565

--- runner/main.go
+++ runner/main.go
@@ -108,6 +108,7 @@ func run(ctx context.Context, args []string, stdout, stderr io.Writer) error {
 		{Bin: "node", Args: []string{"puppeteer/location_write.js"}},
 		{Bin: "node", Args: []string{"puppeteer/form.js"}},
 		{Bin: "node", Args: []string{"puppeteer/cookies.js"}},
+		{Bin: "node", Args: []string{"puppeteer/frame.js"}},
 		//{Bin: "node", Args: []string{"puppeteer/cookies-xhr.js"}}, // FIXME this test doesn't work on ci
 		{Bin: "node", Args: []string{"puppeteer/request_interception.js"}},
 		{Bin: "node", Args: []string{"playwright/connect.js"}},
