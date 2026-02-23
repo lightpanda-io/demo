e2a0d83293b060d30e64dc0254fda54046287fc1
Pierre Tachoire <pierre@lightpanda.io>
2026-02-23 08:50:22 +0100

    add ebay.com product listing integration test

---
+++ integration/ebay_list.js
@@ -0,0 +1,44 @@
+// Copyright 2023-2026 Lightpanda (Selecy SAS)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+'use strict'
+
+import puppeteer from 'puppeteer-core';
+
+const browserAddress = process.env.BROWSER_ADDRESS ? process.env.BROWSER_ADDRESS : 'ws://127.0.0.1:9222';
+
+// use browserWSEndpoint to pass the Lightpanda's CDP server address.
+const browser = await puppeteer.connect({
+    browserWSEndpoint: browserAddress,
+});
+
+// The rest of your script remains the same.
+const context = await browser.createBrowserContext();
+const page = await context.newPage();
+
+await page.goto('https://www.ebay.com/b/Nigel-Sylvester-x-Air-Jordan-1-OG-Low-Better-With-Time/15709/bn_7124881158', {});
+
+const items = await page.evaluate(() => {
+  return Array.from(document.querySelectorAll('.brwrvr__item-results h3')).map(row => {
+    return row.textContent;
+  });
+});
+
+await page.close();
+await context.close();
+await browser.disconnect();
+
+if (items.length < 20) {
+  console.log("Invalid items length", items);
+  throw new Error("invalid results");
+}
