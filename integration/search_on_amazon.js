9c57587c695a2f6455e906b3537c5dde7a1721c9
Halil Durak <halildrk@gmail.com>
2026-03-05 15:29:07 +0300

    assert that book with requested name has found after search

--- integration/search_on_amazon.js
+++ integration/search_on_amazon.js
@@ -1,6 +1,7 @@
 import { connect } from "puppeteer-core";
 
 const URL = "https://amazon.com";
+const bookName = "ces jours qui disparaissent";
 
 const browser = await connect({
   browserWSEndpoint: "ws://127.0.0.1:9222/",
@@ -12,8 +13,8 @@ await page.goto(URL, { waitUntil: "domcontentloaded" });
 // Type in search input.
 while (true) {
   try {
-    await page.type("#twotabsearchtextbox", "Ces jours qui disparaissent", {
-      delay: 121,
+    await page.type("#twotabsearchtextbox", bookName, {
+      delay: 218,
     });
     break;
   } catch {
@@ -30,6 +31,14 @@ while (true) {
 await page.keyboard.press("Enter");
 
 await page.waitForNavigation();
+
+const title = await page.evaluate(() =>
+  document
+    .querySelector("div[data-cy=title-recipe] h2 span")
+    .innerText.toLowerCase(),
+);
+
+console.assert(title.includes(bookName));
 
 await page.close();
 await browser.disconnect();
