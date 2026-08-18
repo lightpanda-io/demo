055729b89d2d5f245bdc6e4a2e4a3cc3f481e0a7
Pierre Tachoire <pierre@lightpanda.io>
2026-08-18 17:10:32 +0200

    add a test for LP.configureCDP obeyRobots

---
+++ puppeteer/lp-configure-obey-robots.js
@@ -0,0 +1,46 @@
+// Copyright 2023-2026 Lightpanda (Selecy SAS)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+'use strict'
+
+import puppeteer from 'puppeteer-core';
+import assert from 'assert';
+import { connectBrowser } from './helpers.js'
+
+const url = process.env.URL ? process.env.URL : 'http://127.0.0.1:1234';
+const browser = await connectBrowser();
+
+// The rest of your script remains the same.
+const context = await browser.createBrowserContext();
+const page = await context.newPage();
+const client = page._client();
+
+await client.send('LP.configureCDP', {
+  obeyRobots: true,
+});
+
+
+// human.txt is disallowed by robots.txt
+try {
+  await page.goto(url + "/human.txt", {});
+  throw new Error("No block");
+} catch (err) {
+  assert.equal("RobotsBlocked", err.message.substring(0, 13))
+}
+
+// bot.txt is allowed by robots.txt
+await page.goto(url + "/bot.txt", {});
+
+await page.close();
+await context.close();
+await browser.disconnect();
