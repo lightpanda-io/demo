c3a7ad3081c41a5b61da66e99ecc2793e1e98332
Pierre Tachoire <pierre@lightpanda.io>
2026-08-21 11:59:11 +0200

    add selenium examples

--- runner/main.go
+++ runner/main.go
@@ -200,6 +200,8 @@ func run(ctx context.Context, args []string, stdout, stderr io.Writer) error {
 		// add fetch + tls mostly for proxy
 		{Bin: "node", Args: []string{"puppeteer/dump.js"}, Env: []string{"URL=https://demo-browser.lightpanda.io/campfire-commerce/"}},
 		{Bin: "go", Args: []string{"run", "fetch/main.go", "https://demo-browser.lightpanda.io/campfire-commerce/"}, Dir: "chromedp"},
+		{Bin: "node", Args: []string{"selenium/bidi/demo.js"}, Env: []string{"URL=http://127.0.0.1:1234/campfire-commerce/"}},
+		{Bin: "node", Args: []string{"selenium/bidi/cdp.js"}, Env: []string{"RUNS=2"}},
 	} {
 		if *verbose {
 			t.Stderr = stderr
